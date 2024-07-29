485250a42c5bff6f9e0e52cb9455353e2fe5118c
Nicolas Perez <nbome12@outlook.com>
2024-07-29 13:36:14 -0500

    finalizacion ejercicio 12

--- 12-Eliminando-caracteres.js
+++ 12-Eliminando-caracteres.js
@@ -7,6 +7,33 @@
  *   estén presentes en str1.
  */
 
-const deleteCharacters = (str1, str2) => {
-    
-}
+function diferenciasCadenas(str1, str2) {
+    // Convertimos las cadenas en conjuntos de caracteres únicos
+    let set1 = new Set(str1);
+    let set2 = new Set(str2);
+  
+    // Inicializamos cadenas de salida vacías
+    let out1 = "";
+    let out2 = "";
+  
+    // Buscamos caracteres únicos en str1 que no están en str2
+    set1.forEach(char => {
+      if (!set2.has(char)) {
+        out1 += char;
+      }
+    });
+  
+    // Buscamos caracteres únicos en str2 que no están en str1
+    set2.forEach(char => {
+      if (!set1.has(char)) {
+        out2 += char;
+      }
+    });
+  
+    // Imprimimos los resultados
+    console.log("out1:", out1);
+    console.log("out2:", out2);
+  }
+  
+  // Ejemplo de uso
+  diferenciasCadenas("hola", "mundo");
